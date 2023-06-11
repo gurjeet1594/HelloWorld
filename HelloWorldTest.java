4e28a687a8fe6ac294c6662e649e9d18d2dbde15
gurjeet1594 <55698820+gurjeet1594@users.noreply.github.com>
2023-06-11 12:05:21 -0600

    Create HelloWorldTest.java

---
+++ HelloWorldTest.java
@@ -0,0 +1,12 @@
+@Test
+public void main() throws Exception{                
+    PrintStream originalOut = System.out; // to have a way to undo the binding with your `ByteArrayOutputStream` 
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(bos));
+    // action
+    App.main(null);
+    // assertion
+    Assert.assertEquals("hello world", bos.toString());
+    // undo the binding in System
+    System.setOut(originalOut);
+}
