17cd275a1ee7b53908c199ceaef0ed4dda54897a
gurjeet1594 <55698820+gurjeet1594@users.noreply.github.com>
2023-06-11 15:34:13 -0600

    Update HelloWorldTest.java

--- HelloWorldTest.java
+++ HelloWorldTest.java
@@ -1,23 +1,7 @@
-import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import static org.junit.Assert.assertEquals;
-
 public class HelloWorldTest {
-
-    @Test
-    public void testHelloWorldOutput() {
-        // Redirect System.out to capture the output
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-
-        // Call the main method of HelloWorld class
-        HelloWorld.main(new String[]{});
-
-        // Get the output from System.out
-        String output = outputStream.toString().trim();
-
-        // Verify the output is "Hello world!"
-        assertEquals("Hello world!", output);
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.printHelloWorld();
     }
 }
+
