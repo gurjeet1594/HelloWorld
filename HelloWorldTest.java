18bdec733360a7354d3959ba57793e8ac510d784
gurjeet1594 <55698820+gurjeet1594@users.noreply.github.com>
2023-06-11 12:08:53 -0600

    Update HelloWorldTest.java

--- HelloWorldTest.java
+++ HelloWorldTest.java
@@ -1,12 +1,23 @@
-@Test
-public void main() throws Exception{                
-    PrintStream originalOut = System.out; // to have a way to undo the binding with your `ByteArrayOutputStream` 
-    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-    System.setOut(new PrintStream(bos));
-    // action
-    App.main(null);
-    // assertion
-    Assert.assertEquals("Hello world!", bos.toString());
-    // undo the binding in System
-    System.setOut(originalOut);
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorldOutput() {
+        // Redirect System.out to capture the output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        // Call the main method of HelloWorld class
+        HelloWorld.main(new String[]{});
+
+        // Get the output from System.out
+        String output = outputStream.toString().trim();
+
+        // Verify the output is "Hello world!"
+        assertEquals("Hello world!", output);
+    }
 }
