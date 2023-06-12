8372b67fca069c9349b688df74afaf88b75b893e
gurjeet1594 <55698820+gurjeet1594@users.noreply.github.com>
2023-06-11 21:04:17 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,4 @@
 public class HelloWorld {
-    public void printHelloWorld() {
-        System.out.println("Hello world!");
-    }
-
-    public static void main(String[] args) {
-        HelloWorld hello = new HelloWorld();
-        hello.printHelloWorld();
-    }
-}
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
